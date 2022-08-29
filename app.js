fb8169000ad6e8d59c6b867c19ee2377c29b1ca0
Xienfuegos <56370507+x13nfuegos@users.noreply.github.com>
2022-08-29 17:12:45 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+
+// let nombre, edad;
+  
+//   nombre = prompt ('Decinos tu nombre o nick:');
+  
+//   if (nombre =="") { alert ('Te tenemos que llamar de alguna manera.'); }
+  
+//   else { alert ('Hola '+nombre + ' bienvenido!'); }
+  
+//   edad = prompt ('Cuantos años tenes: ');
+  
+//   if ( edad <=17 ) {alert ('Sos un niño! :p LLama a un adulto para poder continuar');}
+  
+//   else if (edad>=18) {alert (nombre + ' :) vamos a elegir donde salir hoy.');}
+   
+   
+  const evento = ["SANA", "Geek Party Fest", "Puert Piojo / Doc360", "Temaiken XXl"];
+
+  let text = "";
+  for (let i = 0; i < evento.length; i++) {
+    text += evento[i] + " / ";
+    console.log(text);
+  } 
+
+  
+
